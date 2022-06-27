cd9cc4a0db5b20ef0234c06ec95543d722fb6298
Documenter.jl <documenter@juliadocs.github.io>
2022-06-27 10:15:31 +0000

    build based on 44a7124

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VlasovParticleMethods","category":"page"},{"location":"#VlasovParticleMethods","page":"Home","title":"VlasovParticleMethods","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VlasovParticleMethods.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VlasovParticleMethods]","category":"page"}]
+}
