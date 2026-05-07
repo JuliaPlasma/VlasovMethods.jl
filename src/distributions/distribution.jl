40b11200660e6ae1f674188c57023fb9ff53937e
Michael Kraus <michael.kraus@ipp.mpg.de>
2026-05-07 13:14:42 +0200

    Add functors for DistributionFunction.

--- src/distributions/distribution.jl
+++ src/distributions/distribution.jl
@@ -1,2 +1,13 @@
 
 abstract type DistributionFunction{DT,XD,VD} end
+
+(d::DistributionFunction{T})(x::AbstractVector{T}, v::AbstractVector{T}) where {T} = evaluate(d, x, v)
+(d::DistributionFunction{T,XD,0})(x::Vararg{T,XD}) where {T,XD} = evaluate(d, SVector{XD}(x...), missing)
+(d::DistributionFunction{T,0,VD})(v::Vararg{T,VD}) where {T,VD} = evaluate(d, missing, SVector{VD}(v...))
+
+function (d::DistributionFunction{T,XD,VD})(z::Vararg{T,ZD}) where {T,XD,VD,ZD}
+    @assert ZD == XD+VD
+    x = @view z[1:XD]
+    v = @view z[XD+1:XD+VD]
+    d(SVector{XD}(x...), SVector{VD}(v...))
+end
